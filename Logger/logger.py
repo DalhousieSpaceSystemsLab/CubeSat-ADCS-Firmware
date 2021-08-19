d452548fc1696cb8bbe94b6cb97aa61d8456c012
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2021-08-19 16:47:55 -0300

    Changed IMU frequency back and progressed on the logger

--- Logger/logger.py
+++ Logger/logger.py
@@ -1,22 +1,22 @@
 import serial
 s = serial.Serial(port='COM15', baudrate=115200)
-s.write(bytes(b'm')) # enter mode select
+s.flush()
+s.write(bytes(str('m'),'ascii')) # enter mode select
+s.write(bytes(str('3'),'ascii')) # select uart mode
+s.write(bytes(str('5'),'ascii')) # 9600 baudrate
+#s.write(bytes(b'1')) # parity
+#s.write(bytes(b'1')) # 1 stop bit
+#s.write(bytes(b'1')) # "set 1 == idle level"
+#s.write(bytes(b'2')) # output mode level == normal
+#s.write(bytes(b'(1)'))
+#s.write(bytes(b'y'))
 
-s.write(bytes(b'3')) # select uart mode
-s.write(bytes(b'5')) # 9600 baudrate
-s.write(bytes(b'1')) # parity
-s.write(bytes(b'1')) # 1 stop bit
-s.write(bytes(b'1')) # "set 1 == idle level"
-s.write(bytes(b'2')) # output mode level == normal
-s.write(bytes(b'['))
-#s.write(bytes(b'o')) # configure output mode
-#s.write(bytes(b'4')) # output serialized usb data as ascii representation
-
+s.flush()
 print('written successfully')
 cycles = 0
 while s.is_open:
     print(cycles)
-    res = s.read(10)
+    res = s.read(1)
     print(res)
     cycles+=1
 print('port closed')
